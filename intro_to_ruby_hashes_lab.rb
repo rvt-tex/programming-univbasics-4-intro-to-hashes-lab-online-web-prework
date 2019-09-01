497fb6c86a5c9a60bc58cf69eed5ed18a2083be4
Ruel Tingling <rvtingling@yahoo.com>
2019-09-01 18:20:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,11 @@ def my_hash {
   name: "Roger",
   age: 35 
 }
-  # return a valid hash with any key/value pair of your choice
 end
 
-def pioneer
+def pioneer{
+  name: 'Grace Hopper'
+}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
